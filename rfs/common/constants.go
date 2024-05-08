3d0097f2bccba885986edfd6e4f9c5f99c3e3e80
caleberi <calerioluwa@gmail.com>
2024-05-08 23:25:52 +0100

    chore: clean up constant configuration

--- rfs/common/constants.go
+++ rfs/common/constants.go
@@ -2,26 +2,36 @@ package common
 
 import "time"
 
+type BranchInfo struct {
+	Err   error
+	Event string
+}
+
 const (
 	DeletedNamespaceFilePrefix string = "___deleted__"
 	HeartBeat                  Event  = "HeartBeat"
 	GarbageCollection          Event  = "GarbageCollection"
 	PersistMetaData            Event  = "PersistMetaData"
 	PersistOpsLog              Event  = "PersistOpsLog"
+	MasterHeartBeat            Event  = "MasterHeartBeat"
 
 	// chunk server
 	HeartBeatInterval         time.Duration = 30 * time.Second
-	GarbageCollectionInterval time.Duration = 1 * time.Minute
-	PersistMetaDataInterval   time.Duration = 2 * time.Minute
+	GarbageCollectionInterval time.Duration = 30 * time.Minute
+	PersistMetaDataInterval   time.Duration = 10 * time.Hour
+	ChunkMetaDataFileName                   = "chunk.server.meta"
+	ChunkMaxSizeInMb                        = 64
+	ChunkMaxSizeInByte                      = 64 * 1024 * 1024
 
 	// downloadbuffer
 	DownloadBufferItemExpire = 1 * time.Minute
 	DownloadBufferTick       = 15 * time.Second
 
 	// master server
-	ServerHealthCheckInterval time.Duration = 400 * time.Millisecond
+	ServerHealthCheckInterval time.Duration = 60 * time.Second
 	MasterPersistMetaInterval time.Duration = 1 * time.Hour
-	ServerHealthCheckTimeout  time.Duration = 30 * time.Second
+	ServerHealthCheckTimeout  time.Duration = 60 * time.Second
+	MasterMetaDataFileName                  = "master.server.meta"
 
 	// replicationFactor
 	MinimumReplicationFactor = 3
@@ -32,16 +42,4 @@ const (
 	MutationAppend = (iota + 1) << 1
 	MutationWrite
 	MutationPad
-
-	ChunkMetaDataFileName = "chunk.server.meta"
-	ChunkMaxSizeInMb      = 64
-	ChunkMaxSizeInByte    = 64 * 1024 * 1024
-
-	MasterMetaDataFileName          = "master.server.meta"
-	MasterNamespaceMetaDataFileName = "master-namespace.server.meta"
 )
-
-type BranchInfo struct {
-	Err   error
-	Event string
-}
