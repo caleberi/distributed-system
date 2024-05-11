5ed2c40b0edd07944ddbf20fce8b7cdf5264db6b
caleberi <calerioluwa@gmail.com>
2024-05-11 12:15:53 +0100

    feat: add a temporary client to test out servers

--- ytl/main.go
+++ ytl/main.go
@@ -6,33 +6,95 @@ import (
 
 	"github.com/caleberi/distributed-system/rfs/client"
 	"github.com/caleberi/distributed-system/rfs/common"
-	"github.com/caleberi/distributed-system/rfs/utils"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	addr := "127.0.0.1:9090"
-	client := client.NewClient(common.ServerAddr(addr), 30*time.Millisecond, client.Credentials{})
-	handle, err := client.GetChunkHandle("/videos/the-incredible-hulk-4", common.ChunkIndex(0))
-	if err != nil {
-		log.Err(err).Stack().Msg(err.Error())
-		return
-	}
-	log.Info().Msg(fmt.Sprintf("Got a new handle : %v", handle))
-	lease, err := client.GetChunkServers(handle)
-	if err != nil {
-		log.Err(err).Stack().Msg(err.Error())
-		return
-	}
-	log.Debug().Msg(fmt.Sprintf("lease gotten %#v", lease))
+	client := client.NewClient(common.ServerAddr(addr), 30*time.Millisecond)
+	// handle, err := client.GetChunkHandle("/movies/avatar-sky.mp4", common.ChunkIndex(0))
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// log.Info().Msg(fmt.Sprintf("Got a new handle : %v", handle))
+	// lease, err := client.GetChunkServers(handle)
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// log.Debug().Msg(fmt.Sprintf("lease gotten %#v", lease))
 
-	pathInfos, err := client.List("/")
+	// pathInfos, err := client.List("/")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// utils.ForEach(pathInfos, func(v common.PathInfo) {
+	// 	fmt.Println(">> " + v.Path)
+	// })
+
+	// err = client.MkDir("/test-files/js")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+
+	// err = client.CreateFile("/test-files/js/index.js")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// pathInfos, err = client.List("/")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// utils.ForEach(pathInfos, func(v common.PathInfo) {
+	// 	fmt.Println(">> " + v.Path)
+	// })
+
+	// err = client.DeleteFile("/test-files/js/index.js")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+
+	// pathInfos, err = client.List("/")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// utils.ForEach(pathInfos, func(v common.PathInfo) {
+	// 	fmt.Println(">> " + v.Path)
+	// })
+
+	// err = client.CreateFile("/test-files/js/app.js")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+
+	// err = client.RenameFile("/test-files/js/app.js", "/test-files/js/ok.js")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+
+	// pathInfos, err := client.List("/")
+	// if err != nil {
+	// 	log.Err(err).Stack().Msg(err.Error())
+	// 	return
+	// }
+	// utils.ForEach(pathInfos, func(v common.PathInfo) {
+	// 	fmt.Println(">> " + v.Path)
+	// })
+	fileInfo, err := client.GetFile("/movies")
 	if err != nil {
 		log.Err(err).Stack().Msg(err.Error())
 		return
 	}
-	utils.ForEach(pathInfos, func(v common.PathInfo) {
-		fmt.Println(">> " + v.Path)
-	})
+	log.Info().Msg(fmt.Sprintf("FileInfo :=> %v", *fileInfo))
+
 	client.Close()
 }
