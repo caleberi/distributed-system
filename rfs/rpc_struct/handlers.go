a7bc0dedd066a7b31c7a22e415ed8a853c4ea2e0
caleberi <caleberioluwa@gmail.com>
2025-02-06 00:25:32 +0100

    chore: refactor rpc handler

---
+++ rfs/rpc_struct/handlers.go
@@ -0,0 +1,18 @@
+package rpc_struct
+
+const (
+	MRPCGetChunkHandleHandler                    = "MasterServer.RPCGetChunkHandleHandler"
+	MRPCGetPrimaryAndSecondaryServersInfoHandler = "MasterServer.RPCGetPrimaryAndSecondaryServersInfoHandler"
+	MRPCListHandler                              = "MasterServer.RPCListHandler"
+	MRPCMkdirHandler                             = "MasterServer.RPCMkdirHandler"
+	MRPCCreateFileHandler                        = "MasterServer.RPCCreateFileHandler"
+	MRPCDeleteFileHandler                        = "MasterServer.RPCDeleteFileHandler"
+	MRPCRenameHandler                            = "MasterServer.RPCRenameHandler"
+	MRPCGetFileInfoHandler                       = "MasterServer.RPCGetFileInfoHandler"
+	MRPCGetReplicasHandler                       = "MasterServer.RPCGetReplicasHandler"
+
+	CRPCReadChunkHandler   = "ChunkServer.RPCReadChunkHandler"
+	CRPCForwardDataHandler = "ChunkServer.RPCForwardDataHandler"
+	CRPCWriteChunkHandler  = "ChunkServer.RPCWriteChunkHandler"
+	CRPCAppendChunkHandler = "ChunkServer.RPCAppendChunkHandler"
+)
