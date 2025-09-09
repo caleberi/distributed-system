f29031472b77ac06b04b79947fdb7ec57de47af5
caleberi <caleberioluwa@gmail.com>
2025-09-09 08:06:40 +0100

    feat: implement ArchiverManager for file compression and decompression with pipeline management

---
+++ rfs/archive_manager/archiver_manager.go
@@ -0,0 +1,271 @@
+package archivemanager
+
+import (
+	"compress/gzip"
+	"context"
+	"errors"
+	"io"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/caleberi/distributed-system/rfs/common"
+	filesystem "github.com/caleberi/distributed-system/rfs/file_system"
+)
+
+const ZIP_EXT = ".gz"
+
+type ResultInfo struct {
+	Path common.Path
+	Err  error
+}
+
+type CompressPipeline struct {
+	Task   chan common.Path
+	Result chan ResultInfo
+}
+
+type DecompressPipeline struct {
+	Task   chan common.Path
+	Result chan ResultInfo
+}
+
+type ArchiverManager struct {
+	CompressPipeline   CompressPipeline
+	DecompressPipeline DecompressPipeline
+	fileSystem         *filesystem.FileSystem
+	ctx                context.Context
+	cancel             context.CancelFunc
+	wg                 sync.WaitGroup
+}
+
+func NewArchiver(fileSystem *filesystem.FileSystem, numWorkers int) *ArchiverManager {
+	ctx, cancel := context.WithCancel(context.Background())
+	archiver := &ArchiverManager{
+		CompressPipeline: CompressPipeline{
+			Task:   make(chan common.Path, 10),
+			Result: make(chan ResultInfo, 10),
+		},
+		DecompressPipeline: DecompressPipeline{
+			Task:   make(chan common.Path, 10),
+			Result: make(chan ResultInfo, 10),
+		},
+		fileSystem: fileSystem,
+		ctx:        ctx,
+		cancel:     cancel,
+	}
+	archiver.startWorkers(numWorkers)
+	return archiver
+}
+
+func (ac *ArchiverManager) startWorkers(numWorkers int) {
+	compressWorkers := numWorkers / 2
+	if compressWorkers == 0 {
+		compressWorkers = 1
+	}
+	decompressWorkers := numWorkers - compressWorkers
+	if decompressWorkers == 0 {
+		decompressWorkers = 1
+	}
+
+	ac.wg.Add(compressWorkers)
+	for i := 0; i < compressWorkers; i++ {
+		go ac.compressWorker()
+	}
+
+	ac.wg.Add(decompressWorkers)
+	for i := 0; i < decompressWorkers; i++ {
+		go ac.decompressWorker()
+	}
+}
+
+func (ac *ArchiverManager) compressWorker() {
+	defer ac.wg.Done()
+	for {
+		select {
+		case <-ac.ctx.Done():
+			return
+		case p, ok := <-ac.CompressPipeline.Task:
+			if !ok {
+				return
+			}
+			np, err := ac.compress(p)
+			select {
+			case ac.CompressPipeline.Result <- ResultInfo{Path: common.Path(np), Err: err}:
+			case <-ac.ctx.Done():
+				return
+			}
+		}
+	}
+}
+
+func (ac *ArchiverManager) decompressWorker() {
+	defer ac.wg.Done()
+	for {
+		select {
+		case <-ac.ctx.Done():
+			return
+		case p, ok := <-ac.DecompressPipeline.Task:
+			if !ok {
+				return
+			}
+			np, err := ac.decompress(p)
+			select {
+			case ac.DecompressPipeline.Result <- ResultInfo{Path: common.Path(np), Err: err}:
+			case <-ac.ctx.Done():
+				return
+			}
+		}
+	}
+}
+
+func (ac *ArchiverManager) decompress(path common.Path) (string, error) {
+
+	if !strings.HasSuffix(string(path), ZIP_EXT) {
+		return "", errors.New("file does not have .gz extension")
+	}
+
+	sourceFile, err := ac.fileSystem.GetFile(string(path), os.O_RDWR, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer sourceFile.Close()
+
+	decompressor, err := gzip.NewReader(sourceFile)
+	if err != nil {
+		return "", err
+	}
+	defer decompressor.Close()
+
+	uncompressedFilePath, err := removeGzipExtension(string(path))
+	if err != nil {
+		return "", err
+	}
+
+	err = ac.fileSystem.CreateFile(uncompressedFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	decompressedFile, err := ac.fileSystem.GetFile(
+		uncompressedFilePath, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer decompressedFile.Close()
+
+	_, err = io.Copy(decompressedFile, decompressor)
+	if err != nil {
+		ac.fileSystem.RemoveFile(string(path))
+		return "", err
+	}
+
+	stat, err := decompressedFile.Stat()
+	if err != nil || stat.Size() == 0 {
+		ac.fileSystem.RemoveFile(string(path))
+		return "", errors.New("decompressed file is invalid or empty")
+	}
+
+	return uncompressedFilePath, ac.fileSystem.RemoveFile(string(path))
+}
+
+func (ac *ArchiverManager) compress(path common.Path) (string, error) {
+	sourceFile, err := ac.fileSystem.GetFile(string(path), os.O_RDWR, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer sourceFile.Close()
+
+	destinationPath := string(path) + ZIP_EXT
+	_, err = ac.fileSystem.GetStat(destinationPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err := ac.fileSystem.CreateFile(destinationPath)
+			if err != nil {
+				return "", err
+			}
+		} else {
+			return "", err
+		}
+	}
+
+	compressedFile, err := ac.fileSystem.GetFile(
+		destinationPath, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer compressedFile.Close()
+
+	compressor := gzip.NewWriter(compressedFile)
+	_, err = io.Copy(compressor, sourceFile)
+	if err != nil {
+		ac.fileSystem.RemoveFile(destinationPath)
+		return "", err
+	}
+
+	if err := compressor.Close(); err != nil {
+		ac.fileSystem.RemoveFile(destinationPath)
+		return "", err
+	}
+
+	return destinationPath, ac.fileSystem.RemoveFile(string(path))
+}
+
+func (ac *ArchiverManager) Close() {
+	defer func() {
+		ac.cancel()
+
+		close(ac.CompressPipeline.Task)
+		close(ac.DecompressPipeline.Task)
+
+		ac.wg.Wait()
+
+		close(ac.CompressPipeline.Result)
+		close(ac.DecompressPipeline.Result)
+	}()
+
+	timeout := time.After(10 * time.Second)
+	for {
+		compressLen := len(ac.CompressPipeline.Task)
+		decompressLen := len(ac.DecompressPipeline.Task)
+		if compressLen == 0 && decompressLen == 0 {
+			break
+		}
+		select {
+		case <-timeout:
+			return
+		case <-time.After(100 * time.Millisecond):
+			continue
+		}
+	}
+
+}
+func (ac *ArchiverManager) SubmitCompress(path common.Path) error {
+	select {
+	case ac.CompressPipeline.Task <- path:
+		return nil
+	case <-time.After(5 * time.Second):
+		return errors.New("timeout submitting compression task")
+	case <-ac.ctx.Done():
+		return errors.New("archiver is shutting down")
+	}
+}
+
+func (ac *ArchiverManager) SubmitDecompress(path common.Path) error {
+	select {
+	case ac.DecompressPipeline.Task <- path:
+		return nil
+	case <-time.After(5 * time.Second):
+		return errors.New("timeout submitting decompression task")
+	case <-ac.ctx.Done():
+		return errors.New("archiver is shutting down")
+	}
+}
+
+func removeGzipExtension(path string) (string, error) {
+	if !strings.HasSuffix(path, ZIP_EXT) {
+		return "", errors.New("file does not have .gz extension")
+	}
+	return strings.TrimSuffix(path, ZIP_EXT), nil
+}
---
+++ rfs/archive_manager/archiver_manager_test.go
@@ -0,0 +1,213 @@
+package archivemanager
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/caleberi/distributed-system/rfs/common"
+	filesystem "github.com/caleberi/distributed-system/rfs/file_system"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	dataFile = "./data/source.txt"
+	zipExt   = ".gz"
+	timeout  = 5 * time.Second
+)
+
+var (
+	filepaths = []string{
+		"test_data2/test_data1/test_1.txt",
+		"test_data2/test_1.txt",
+		"test_data2/test_data1/test_data3/test_2.txt",
+		"test_data1/test_2.txt",
+	}
+	dirpaths = []string{
+		"test_data1",
+		"test_data1/test_data",
+		"test_data2/test_data1/test_data3",
+	}
+)
+
+func testSetup(t *testing.T) (*filesystem.FileSystem, *ArchiverManager, []byte, string) {
+	t.Helper()
+
+	data, err := os.ReadFile(dataFile)
+	assert.NoError(t, err)
+
+	tempDir := t.TempDir()
+	fsys := filesystem.NewFileSystem(tempDir)
+
+	for _, dir := range dirpaths {
+		assert.NoErrorf(t, fsys.MkDir(dir), "failed to create directory %s", dir)
+	}
+
+	for _, path := range filepaths {
+		assert.NoErrorf(t, fsys.CreateFile(path), "failed to create file %s: %v", path, err)
+	}
+
+	for _, path := range filepaths {
+		f, err := fsys.GetFile(path, os.O_RDWR|os.O_TRUNC, 0644)
+		assert.NoError(t, err, "failed to retrieve file %s: %v", path, err)
+		n, err := f.Write(data)
+		assert.NoError(t, err)
+		assert.GreaterOrEqual(t, n, 0)
+		assert.NoError(t, f.Close(), "failed to close file %s: %v", path, err)
+	}
+
+	archiver := NewArchiver(fsys, 2)
+	return fsys, archiver, data, tempDir
+}
+
+func TestMain(m *testing.M) {
+	if _, err := os.Stat(dataFile); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(dataFile), 0755)
+		os.WriteFile(dataFile, []byte("test data for compression"), 0644)
+	}
+	os.Exit(m.Run())
+}
+
+type pathStat struct {
+	size int64
+	path string
+}
+
+func TestArchiveCompressWithFileSystem(t *testing.T) {
+	fsys, archiver, _, _ := testSetup(t)
+	defer archiver.Close()
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	pathStats := make(map[common.Path]pathStat)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, path := range filepaths {
+			p := common.Path(path)
+			info, err := fsys.GetStat(path)
+			assert.NoError(t, err)
+			if info != nil {
+				mu.Lock()
+				pathStats[p] = pathStat{size: info.Size(), path: path}
+				mu.Unlock()
+			}
+			assert.NoError(t, archiver.SubmitCompress(p))
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for range filepaths {
+			select {
+			case result := <-archiver.CompressPipeline.Result:
+				assert.NoError(t, result.Err)
+				assert.NotEmpty(t, result.Path)
+				info, err := fsys.GetStat(string(result.Path))
+				assert.NoError(t, err)
+				assert.True(t, info.Size() >= 0)
+				assert.True(t, strings.HasSuffix(string(result.Path), zipExt))
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+}
+
+func TestArchiveDecompressionWithFileSystem(t *testing.T) {
+	fsys, archiver, _, _ := testSetup(t)
+	defer archiver.Close()
+
+	for _, path := range filepaths {
+		assert.NoError(t, archiver.SubmitCompress(common.Path(path)))
+	}
+
+	go func() {
+		for result := range archiver.CompressPipeline.Result {
+			assert.NoError(t, result.Err)
+			info, err := fsys.GetStat(string(result.Path))
+			assert.NoError(t, err)
+			assert.True(t, info.Mode().IsRegular())
+			assert.NoError(t, archiver.SubmitDecompress(result.Path))
+		}
+	}()
+
+	go func() {
+		for result := range archiver.DecompressPipeline.Result {
+			assert.NoError(t, result.Err)
+			assert.True(t, !strings.HasSuffix(string(result.Path), zipExt))
+			info, err := fsys.GetStat(string(result.Path))
+			assert.NoError(t, err)
+			assert.True(t, info.Mode().IsRegular())
+			f, err := fsys.GetFile(string(result.Path), os.O_RDONLY, 0644)
+			assert.NoError(t, err)
+			assert.NoError(t, f.Close())
+		}
+	}()
+}
+
+func TestNormalCompression(t *testing.T) {
+	fsys, archiver, originalData, _ := testSetup(t)
+
+	tests := []struct {
+		name      string
+		path      string
+		content   []byte
+		expectErr bool
+	}{
+		{
+			name:      "valid file",
+			path:      "test.txt",
+			content:   originalData,
+			expectErr: false,
+		},
+		{
+			name:      "empty file",
+			path:      "empty.txt",
+			content:   []byte{},
+			expectErr: false,
+		},
+		{
+			name:      "non-existent file",
+			path:      "nonexistent.txt",
+			content:   nil,
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.content != nil {
+				err := fsys.CreateFile(tt.path)
+				assert.NoError(t, err)
+				f, err := fsys.GetFile(tt.path, os.O_RDWR|os.O_TRUNC, 0644)
+				assert.NoError(t, err)
+				_, err = f.Write(tt.content)
+				assert.NoError(t, err)
+				assert.NoError(t, f.Close())
+			}
+
+			newPath, err := archiver.compress(common.Path(tt.path))
+			if tt.expectErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.True(t, strings.HasSuffix(newPath, zipExt))
+			info, err := fsys.GetStat(newPath)
+			assert.NoError(t, err)
+			assert.False(t, info.Size() == 0)
+		})
+	}
+}
